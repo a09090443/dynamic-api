f63f9f64dfccfa2e489ba005d844473ee1d5a79a
Gary <zipe.daden@gmail.com>
2024-07-24 00:06:11 +0800

    Base jar init.

---
+++ base-jar/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.dynamicapi"
+version = "0.0.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.a09090443:base-spring-boot-starter:3.2.5.4")
+    implementation("io.github.a09090443:db-spring-boot-starter:3.2.5.1")
+    implementation("net.sf.jasperreports:jasperreports:6.20.6")
+    implementation("com.github.librepdf:openpdf:2.0.2")
+
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+}
---
+++ base-jar/src/main/java/com/dynamicapi/webservice/BaseWebservice.java
@@ -0,0 +1,13 @@
+package com.dynamicapi.webservice;
+
+import com.dynamicapi.dao.MockResponseDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class BaseWebservice {
+    protected MockResponseDao mockResponseDao;
+
+    @Autowired
+    public final void setMockResponseDao(MockResponseDao mockResponseDao) {
+        this.mockResponseDao = mockResponseDao;
+    }
+}
